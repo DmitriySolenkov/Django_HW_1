17aa0f7a92d4bb44e59b499149211e548b269789
kreoxLaptop <soldimksol@gmail.com>
2023-10-28 18:45:13 +0300

    HW5

--- myapp2/admin.py
+++ myapp2/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import User, Product, Order
 
-# Register your models here.
+
+@admin.action(description="Заказ выполнен")
+def order_completed(modeladmin, request, queryset):
+    queryset.update(is_completed=True)
+
+
+@admin.action(description="Все по 10$")
+def sale_10(modeladmin, request, queryset):
+    queryset.update(price=10)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'amount']
+    ordering = ['amount']
+    search_fields = ['name']
+    search_help_text = 'Поиск по названию продукта'
+    actions = [sale_10]
+    fields = ['name', 'description', 'price', 'amount', 'addition_date']
+    readonly_fields = ['addition_date']
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone_number', 'addres']
+    list_filter = ['addres']
+    search_fields = ['name']
+    search_help_text = 'Поиск по имени пользователя'
+    readonly_fields = ['registration_date']
+    fieldsets = [
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['name'],
+            },
+        ),
+        (
+            'Личные данные',
+            {
+                'classes': ['collapse'],
+                'description': 'Личные данные пользователя',
+                'fields': ['email', 'phone_number', 'addres'],
+            },
+        ),
+        (
+            None,
+            {
+                'classes': ['wide'],
+                'fields': ['registration_date'],
+            },
+        ),
+    ]
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'total_price',
+                    'date_ordered', 'is_completed']
+    ordering = ['is_completed', 'date_ordered']
+    list_filter = ['is_completed', 'total_price']
+    actions = [order_completed]
+    fields = ['customer', 'total_price', 'date_ordered', 'is_completed']
+    readonly_fields = ['date_ordered']
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
--- myapp2/models.py
+++ myapp2/models.py
@@ -4,24 +4,33 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
-    phone_number = models.IntegerField()
+    phone_number = models.PositiveIntegerField()
     addres = models.TextField()
     registration_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'Username: {self.name}, email: {self.email}, phone number: {self.phone_number}, addres: {self.addres}, registration date: {self.registration_date}'
+        return f'{self.name}, {self.email}, {self.phone_number}, {self.addres}'
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
-    price = models.DecimalField(max_digits=8, decimal_places=2)
-    amount = models.IntegerField()
+    description = models.TextField(default='', blank=True)
+    price = models.DecimalField(
+        default=999999.99, max_digits=8, decimal_places=2)
+    amount = models.PositiveSmallIntegerField(default=0)
     addition_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.name}, {self.description}, {self.price}, {self.amount}'
 
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_price = models.DecimalField(
+        default=999999.99, max_digits=8, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.customer.name}, {self.total_price}, {self.date_ordered}, {self.is_completed}'
